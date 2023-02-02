dd1503b66d6a1b446dc477e07efa0da98679f9be
Débora <deboradoj.93@gmail.com>
2023-02-02 21:39:31 +0000

    implementado componente de highlights

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Highlights from "./Components/Highlights";
 import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Highlights />
     </div>
   );
 }
