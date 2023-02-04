9168a896877848a6a351b1898ada7edd6742db35
Débora <deboradoj.93@gmail.com>
2023-02-04 12:07:13 +0000

    Implementado componente de Produtos e a sua estilização

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 import Highlights from "./Components/Highlights";
 import Navbar from "./Components/Navbar";
+import Products from "./Components/Products";
 
 function App() {
+  const [showProducts, setShowProducts] = useState(true);
+
   return (
     <div className="App">
       <Navbar />
-      <Highlights />
+      {!showProducts && <Highlights />}
+      {showProducts && <Products />}
     </div>
   );
 }
